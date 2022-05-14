2c4b32baf95a0d674a2004bd0f886c3b8d019faf
Adam Skladowski <a_skl39@protonmail.com>
2022-05-14 11:27:16 +0200

    config/dtb: Add 10.or G

---
+++ config/tenor_g.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[hx8399c_auo_53_1080p_video]="tenor,hx8399c_auo"
+	[ili7807d_djn_53_1080p_video]="tenor,ili7807d_djn"
+	[ili7807d_djn_auo_53_1080p_video]="tenor,ili7807d_djn_auo"
+)
