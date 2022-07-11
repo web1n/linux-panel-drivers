3947d1a3a78376f05113b809035a65a2827a3602
fekz115 <fekz115@gmail.com>
2022-07-11 22:19:19 +0300

    config/dtb: Add Xiaomi Redmi 5 Plus (vince)

---
+++ config/xiaomi-vince.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[td4310_fhdplus_video_e7]="xiaomi,td4310-fhdplus-e7"
+	[td4310_fhdplus_video_e7_g55]="xiaomi,td4310-fhdplus-e7-g55"
+	[td4310_ebbg_fhdplus_video_e7]="xiaomi,td4310-ebbg-fhdplus-e7"
+	[nt36672_tianma_fhdplus_video_e7]="xiaomi,nt36672-tianma-fhdplus-e7"
+	[nt36672_csot_fhdplus_video_e7]="xiaomi,nt36672-csot-fhdplus-e7"
+)
