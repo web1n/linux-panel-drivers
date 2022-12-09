8dcbe819155e14414bcd70e9639c241dc039dae0
nathanjensen1 <95645648+nathanjensen1@users.noreply.github.com>
2022-12-09 14:23:43 -0700

    config: motorola-potter: Fix typo in Tianma panel name
    Signed-off-by: Nathan Jensen <nathanjensen1@outlook.com>

--- config/motorola-potter.sh
+++ config/motorola-potter.sh
@@ -3,6 +3,6 @@
 
 OPTIONS=(-r vsn -r vsp)
 PANELS=(
-	[tianma_520_1080p_vid_v0]="tianma,tl052vdpx02"
+	[tianma_520_1080p_vid_v0]="tianma,tl052vdxp02"
 	[boe_520_1080p_vid_v0]="boe,bs052fhm-a00-6c01"
 )
