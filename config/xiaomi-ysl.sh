8c5350facfd9289e063bebe9563c1356def50c44
Barnabás Czémán <trabarni@gmail.com>
2023-05-14 18:37:26 +0200

    config/dtb: Add Xiaomi Redmi S2/Y2 (ysl)

---
+++ config/xiaomi-ysl.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[auo_hd720_video]="xiaomi,ysl-ili7807d"
+	[sc_ili9881c_hd720_video]="xiaomi,ysl-ili9881c"
+	[truly_hd720_video]="xiaomi,ysl-hx8394f"
+)
